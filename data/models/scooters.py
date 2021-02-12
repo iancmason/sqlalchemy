3eab1815523b8571b96474e0776a8d5eb62ef022
Ian <ian.christopher.mason@gmail.com>
2021-02-11 20:12:22 -0600

    add rest of classes

--- data/models/scooters.py
+++ data/models/scooters.py
@@ -6,9 +6,9 @@ class Scooter(SqlAlchemyBase):
     __tablename__ = 'scooters'    
 
     id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
-    created_date = sa.Column(sa.DateTime)
-    vin = sa.Column(sa.String)
-    model = sa.Column(sa.String)
-    battery_level = sa.Column(sa.Integer)
+    created_date = sa.Column(sa.DateTime, default=datetime.datetime.now)
+    vin = sa.Column(sa.String, index=True, unique=True)
+    model = sa.Column(sa.String, index=True)
+    battery_level = sa.Column(sa.Integer, index=True)
 
     # TODO: Relationships
