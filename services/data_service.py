0f3b9b4c64cee935538e79a0a1460e5a660f6201
Ian <ian.christopher.mason@gmail.com>
2021-02-17 15:42:58 -0600

    more queries

--- services/data_service.py
+++ services/data_service.py
@@ -24,21 +24,46 @@ def get_default_user():
     return user
 
 def book_scooter(scooter: Scooter, user: User, start_date: datetime.datetime) -> Rental:
-    rental = None
+    session = session_factory.create_session()
+
+    scooter = session.query(Scooter).filter(Scooter.id == scooter.id).one()
+    scooter.location_id = None
+    scooter.battery_level = random.randint(50, 100)
+
+    rental = Rental()
+    rental.scooter_id = scooter.id
+    rental.user_id = user.id
+    rental.start_time = start_date
+    rental.end_time = rental.start_time + datetime.timedelta(days=1)
+
+    session.add(rental)
+    session.commit()
 
     return rental
 
 def park_scooter(scooter_id: int, location_id: int) -> Scooter:
-    scooter = None
+    session = session_factory.create_session()
+
+    scooter = session.query(Scooter).filter(Scooter.id == scooter_id).one()
+    scooter.location_id = location_id
+    scooter.battery_level = 100
+
+    session.commit()
 
     return scooter
 
 def rented_scooters() -> List[Scooter]:
-    scooters = []
+    session = session_factory.create_session()
+
+    #noinspection PyComparisonWithNone
+    scooters = session.query(Scooter).filter(Scooter.id == None).all()
 
     return list(scooters)
 
 def parked_scooters() -> List[Scooter]:
-    scooters = []
+    session = session_factory.create_session()
+
+    # noinspection PyComparisonWithNone
+    scooters = session.query(Scooter).filter(Scooter.id != None).all()
 
     return list(scooters)
